99737dd108f0fd7765904c40ca776b0deec68d29
NuuttiBoi <104026912+NuuttiBoi@users.noreply.github.com>
2025-12-19 14:18:49 +0200

    Cors policy fixed

--- backend/server/streamServer.js
+++ backend/server/streamServer.js
@@ -1,7 +1,14 @@
 import express from 'express';
 import { spawn } from "child_process";
 
+import cors from "cors";
+
 const app = express();
+app.use(cors({
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 let ffmpegProcess = null;
 
 app.post('/start-stream', (req, res) => {
@@ -11,9 +18,9 @@ app.post('/start-stream', (req, res) => {
 
   // Ffmpeg location argument needs to be changed based on where ffmpeg is located
   // Also the video input device name needs to be changed based on the system
-  ffmpegProcess = spawn('C:/Program Files/ffmpeg/bin/ffmpeg.exe', [
+  ffmpegProcess = spawn('C:\\Users\\nuutt\\Downloads\\ffmpeg-2025-12-18-git-78c75d546a-essentials_build\\ffmpeg-2025-12-18-git-78c75d546a-essentials_build\\bin\\ffmpeg.exe', [
     '-f', 'dshow',
-    '-i', 'video=Integrated Webcam',
+    '-i', 'video=Integrated Camera',
     '-pix_fmt', 'yuv420p',
     '-preset', 'veryfast',
     '-tune', 'zerolatency',
