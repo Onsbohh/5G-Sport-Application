18166e011a3d65869e8047a74999e30b83587bcd
onni pajula <onni.pajula@metropolia.fi>
2025-12-30 17:09:05 +0200

    Fetches sensor data while stream live.

--- frontend/src/components/LiveStream.js
+++ frontend/src/components/LiveStream.js
@@ -1,7 +1,8 @@
 import ReactPlayer from "react-player"
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
 import '../styles/LiveStream.css';
+import {getHeartRateByTimestamp, getEcgByTimestamp} from "../service/sensorDataService"
 
 export default function LiveStream() {
     const [streaming, setStreaming] = useState(false);
@@ -16,13 +17,37 @@ export default function LiveStream() {
         }
     }
 
+    // Fetch sensor data every second when streaming on current timestamp
+    const fetchSensorData = async () => {
+        try {
+            const timestamp = Math.floor(Date.now() / 1000); // Current timestamp in seconds
+            console.log("Fetching sensor data for timestamp: ", timestamp);
+            const heartRateData = await getHeartRateByTimestamp(timestamp);
+            const ecgData = await getEcgByTimestamp(timestamp);
+            console.log("Heart Rate Data: ", heartRateData);
+            console.log("ECG Data: ", ecgData);
+        }
+        catch (error) {
+            console.error("Error fetching sensor data: ", error);
+        }
+    }
+
+    useEffect(() => {
+        if (streaming) {
+            const interval = setInterval(() => {
+                fetchSensorData().then(r => console.log("Sensor data fetched"));
+            }, 1000); // Fetch data every second
+            return () => clearInterval(interval);
+        }
+    }, [streaming]);
+
     return (
         <div style={{width: "100%", maxWidth: "1200px", margin: "0 auto"}}>
             <div className={`stream-container ${!streaming ? "offline" : ""}`}>
                 {streaming ? (
                     <iframe
                         src={`http://localhost:8889/camstream?_=${Date.now()}`}
-                        allow="autoplay; fullscreen"
+                        allow="fullscreen"
                         style={{width: "100%", height: "100%", borderRadius: "8px", border: "none"}}
                     />
                 ) : (
