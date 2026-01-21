242fe3145352358a563493e8284ef23b8b0a28cf
onni pajula <onni.pajula@metropolia.fi>
2026-01-21 15:52:17 +0200

    Added first attempt for ecg graph.

--- frontend/src/components/DataPanel.js
+++ frontend/src/components/DataPanel.js
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
 import stylesheet from '../styles/DataPanel.css';
+import { CartesianGrid, Line, LineChart, XAxis, YAxis } from 'recharts';
 
 export default function DataPanel({ title, sensorData }) {
-
+    let ecgData = [];
     console.log("Sensors in DataPanel: ", sensorData);
     if (sensorData.heart_rate.value) {
         console.log(sensorData.heart_rate.value.Average_BPM);
+    }
+
+    if (sensorData.ecg.value) {
+        ecgData = sensorData.ecg.value.Samples.map((sample, index) => ({
+            Samples: index,
+            ecg: sample,
+        }));
     }
 
     return (
@@ -20,7 +28,12 @@ export default function DataPanel({ title, sensorData }) {
             )}
             {sensorData.ecg.value ? (
                 <div>
-                    <p><strong>ECG Data:</strong> {JSON.stringify(sensorData.ecg.value.Samples)}</p>
+                    <LineChart width={400} height={200} data={ecgData}>
+                        <CartesianGrid stroke="#ccc" />
+                        <XAxis dataKey="Samples" />
+                        <YAxis />
+                        <Line type="monotone" dataKey="ecg" stroke="#8884d8" dot={false} strokeWidth={2}/>
+                    </LineChart>
                 </div>
             ) : (
                 <p>No ECG data available.</p>
--- frontend/src/service/httpService.js
+++ frontend/src/service/httpService.js
@@ -5,5 +5,7 @@ const http = axios.create({
     baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8080/api",
 });
 
+console.log("HTTP Service Base URL:", http.defaults.baseURL);
+
 export default http
 
